2de397122d34fe3e0c3a07aca56dd97cb69a3d02
JimiLinn <70435082+JimiLinn@users.noreply.github.com>
2020-11-03 14:38:57 +0200

    ex3
    ex3 Ready

--- untitled/mainwindow.cpp
+++ untitled/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,12 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    ui=nullptr;
 }
+
+void MainWindow::on_btnSayHello_clicked()
+{
+    QString name=ui->lineEditName->text();
+    ui->labelResult->setText("Terve " +name);
+}
+
