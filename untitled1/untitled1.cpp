ef1bc4405d9dbd42e6ffe9dfc1093b783889cb8e
JimiLinn <70435082+JimiLinn@users.noreply.github.com>
2020-11-09 13:53:05 +0200

    ex5
    ex5 ready

---
+++ untitled1/untitled1.cpp
@@ -0,0 +1,21 @@
+#include "untitled1.h"
+
+
+int Untitled1::getAge() const
+{
+    return age;
+}
+
+void Untitled1::setAge(int value)
+{
+    age = value;
+}
+
+int Untitled1::getBornYear()
+{
+    return 2020-this->age;
+}
+
+Untitled1::Untitled1()
+{
+}
---
+++ untitled1/untitled1.h
@@ -0,0 +1,17 @@
+#ifndef UNTITLED1_H
+#define UNTITLED1_H
+
+#include "untitled1_global.h"
+
+class UNTITLED1SHARED_EXPORT Untitled1
+{
+private:
+    int age;
+public:
+    Untitled1();
+    int getAge() const;
+    void setAge(int value);
+    int getBornYear();
+};
+
+#endif // UNTITLED1_H
